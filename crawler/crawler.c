aaf68fef6a24d5d8f37272e85badcc42d790dfa4
Paul Chirkov <f006h0q@plank.thayer.dartmouth.edu>
2023-10-14 15:38:56 -0400

    crawler documentation

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,4 +1,11 @@
 
+/* crawler.C 
+ * 
+ * A program for crawling a webpage and gathering all the urls on it. 
+ *
+ * Paul Chirkov October 2023
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <webpage.h>
@@ -12,9 +19,9 @@ static void parseArgs(const int argc, char* argv[],char** seedURL, char** pageDi
 static void crawl(char* seedURL, char* pageDirectory, const int maxDepth);
 static void pageScan(webpage_t* page, bag_t* pagesToCrawl, hashtable_t* pagesSeen);
 
-
 /**************** main ****************/
 int main(const int argc, char* argv[]){
+
     char* seedURL = NULL;
     char* pageDirectory = NULL;
     int maxDepth =0;
@@ -25,52 +32,66 @@ int main(const int argc, char* argv[]){
     exit(0);
 }
 
-
 /**************** parseArgs ****************/
-/*
-*
+/* Handles command line arguments, makes sure they are all valid. 
+* 
+* Caller provides:
+*   num arguments, array of arguments, pointers to variables that should be updated by arguments
 * We guarantee: 
-*   The arguments are valid if doesn't exit
+*   If the function doesn't exit non-zero, the arguments are valid
+* We return:
+*   only return when succesful, otherwise exit non-zero
 */
 static void parseArgs(const int argc, char* argv[],char** seedURL, char** pageDirectory, int* maxDepth){
+    
     const char* progName = argv[0]; // name of this program
-
-    //make sure there is a correct number of arguments
+    // make sure there is a correct number of arguments
     if (argc != 4) {
         fprintf(stderr, "usage: %s seedURL pageDirectory maxDepth\n", progName);
         exit(1);
     }
 
-    //normalize the url and make sure it is internal
+    // normalize the url and make sure it is internal
     char* URLcheck = normalizeURL(argv[1]);
-
     if(!isInternalURL(URLcheck)){
         fprintf(stderr, "Must use an internal URL.\n");
         exit(2);
     }
     *seedURL = URLcheck;
 
-    //make sure that pageDirectory is a valid path
+    // make sure that pageDirectory is a valid path
     if(!pagedir_init(argv[2])){
         fprintf(stderr, "the specified directory does not exist.\n");
         exit(3);
     }
     *pageDirectory = argv[2];
     
-    //CALL MAX NUM. CHECK THAT THIS SHIT IS A NUMBER AND NOT SOME STRING!!!!!!!
-    //check that maxDepth is a number and greater than 0.
-
-    int num = atoi(argv[3]);
+    // check third argument is a number.
+    int num = -1;
+    if(sscanf(argv[3], "%d", &num) != 1){
+       fprintf(stderr, "Max depth must be a number in the range [0,10].\n");
+        exit(4); 
+    }
+    // check that the number is in valid range
     if(!(num >= 0 && num <=10)){
         fprintf(stderr, "Max depth must be in the range [0,10].\n");
         exit(4);
     }
     
-    //maybe needs to be dereferenced?
     *maxDepth = num;    
 }
 
 /**************** crawl ****************/
+/* Crawls the webpage at the seed URL and all embedded URLS to given depth, saves page to pageDirectory.  
+*
+* Caller Provides:
+*   starting URL, existing directory to store pages and the max search depth.
+* We do: 
+*   Extract webpage from bag. Fetch HTML and save it if succesful.
+*   Add all urls in page to bag if valid depth and then repeat. 
+* Notes:
+*   The memory allocated to new webpages, the bag and the hashtable are all freed by their respective delete() functions.
+*/
 static void crawl(char* seedURL, char* pageDirectory, const int maxDepth){
 
     //initialize the hashtable and add the seedURL
@@ -78,7 +99,7 @@ static void crawl(char* seedURL, char* pageDirectory, const int maxDepth){
     hashtable_insert(hashtable, seedURL, "");
     //initialize the bag 
     bag_t* bag = bag_new();
-    //add a webpage representing the seedURL at depth 0 
+    //create and add webpage representing the seedURL at depth 0 
     webpage_t* page = webpage_new(seedURL, 0, NULL);
     bag_insert(bag, page);
 
@@ -86,53 +107,58 @@ static void crawl(char* seedURL, char* pageDirectory, const int maxDepth){
     webpage_t* current;
     //while bag is not empty 
     while((current = bag_extract(bag)) != NULL){
-    //  pull a webpage from the bag 
-    //	fetch the HTML for that webpage
-        //	if fetch was successful,
+        //pull a webpage from the bag 
+        //fetch the HTML for that webpage and if fetch was successful
         if(webpage_fetch(current)){
-    //		save the webpage to pageDirectory
+            //save the webpage to pageDirectory
             pagedir_save(current, pageDirectory, docID++);
-    //		if the webpage is not at maxDepth,
+            //if the webpage is not at maxDepth,
             if(webpage_getDepth(current) < maxDepth){
-    //			pageScan that HTML
+                //pageScan that HTML
                 pageScan(current, bag, hashtable);
             }
         }
-    //delete that webpage.. Don't want to delete if it is already freed. 
+        //delete webpage, free memory
         webpage_delete(current);
     }
-    //delete the hashtable
+    //delete hashtable
     hashtable_delete(hashtable, NULL);
-    //delete the bag
+    //delete bag
     bag_delete(bag, NULL);
 }
 
 /**************** pageScan ****************/
+/* Function for adding all embedded urls to the bag if they are valid.
+*
+* Caller provides:
+*   Page to scan, bag to add new pages, hashtable of URLS already seen
+* We guarantee: 
+*   If URL is not internal or already in the hashtable, the memory that holds it is freed. 
+*/
 static void pageScan(webpage_t* page, bag_t* pagesToCrawl, hashtable_t* pagesSeen){
     
     int pos = 0;
     char* result;
     //while there is another URL in the page
     while((result = webpage_getNextURL(page, &pos)) != NULL){
-    //	normalize URL
+        //normalize URL
         char* normalizedResult = normalizeURL(result);
         free(result);
         //check if it is internal
         if(isInternalURL(normalizedResult)){
-    //		insert the webpage into the hashtable
-            //do I INSSERT THE RESULT INTO HASHTABLE???????
+            //insert the webpage into the hashtable
             if(hashtable_insert(pagesSeen, normalizedResult, "")){
-    //			create a webpage_t for it.
+                //create a webpage_t for it.
                 webpage_t* newPage = webpage_new(normalizedResult, webpage_getDepth(page) + 1, NULL);
-    //			insert the webpage into the bag
+                //insert the webpage into the bag
                 bag_insert(pagesToCrawl, newPage);
             }
             else{
-                free(normalizedResult); // free URL only if it fails to be inserted into hashtable
+                free(normalizedResult); // free URL if it fails to be inserted into hashtable
             } 
         }
         else{
-            free(normalizedResult);
+            free(normalizedResult); // free if it's not internal.
         }
     }
 }
